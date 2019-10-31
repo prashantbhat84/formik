7624e88da56dbab03fa08160b498382e1fde8562
prashantbhat84 <prashantbhat84@gmail.com>
2019-10-31 12:35:55 +0530

    basic form done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
-import React, { Fragment } from 'react';
-import { withFormik } from 'formik';
+import React from 'react';
+import { withFormik, Form, Field } from 'formik';
 import Yup from 'yup';
-const App = ({ values, handleChange, handleSubmit }) => {
+const App = ({ values }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type='email'
-        name='email'
-        placeholder='E-mail'
-        value={values.email}
-        onChange={handleChange}
-      />
+    <Form>
+      <Field type='email' name='email' placeholder='E-mail' />
       <br />
-      <input
-        type='password'
-        name='password'
-        placeholder='password'
-        value={values.password}
-        onChange={handleChange}
-      />
-      <button onSubmit={() => {}}>Submit</button>
-    </form>
+      <Field type='password' name='password' placeholder='password' />
+      <br />
+      <label>
+        <Field type='checkbox' name='newsletter' checked={values.newsletter} />
+        Subscribe to our weekly updates
+      </label>
+      <br />
+      <label>
+        Plan Type
+        <Field component='select' name='plan'>
+          <option value='Free'>Free</option>
+          <option value='Premium'>Premium</option>
+        </Field>
+      </label>
+      <br />
+      <button onSubmit={() => {}} type='submit'>
+        Submit
+      </button>
+    </Form>
   );
 };
 
 const FormikApp = withFormik({
-  mapPropsToValues({ email, password }) {
+  mapPropsToValues({ email, password, newsletter, plan }) {
     return {
       email: email || '',
-      password: password || ''
+      password: password || '',
+      newsletter: newsletter || false,
+      plan: plan || 'Free'
     };
   },
   handleSubmit(values) {
